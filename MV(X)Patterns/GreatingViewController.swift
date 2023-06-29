335b0fe50a5b81e78ce5052fc7322ab130266e4b
Dinar Khakimov <in_order@icloud.com>
2023-06-29 11:35:36 +0500

    Initial Commit

---
+++ MV(X)Patterns/GreatingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MV(X)Patterns
+//
+//  Created by Dinar Khakimov on 29.06.2023.
+//
+
+import UIKit
+
+class GreatingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MV(X)Patterns/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  MV(X)Patterns
+//
+//  Created by Dinar Khakimov on 29.06.2023.
+//
+
+import Foundation
