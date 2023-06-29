f0b0075b78b457384e784e180fb4a05a25844966
Dinar Khakimov <in_order@icloud.com>
2023-06-29 14:44:13 +0500

    completed

--- MV(X)Patterns/GreatingPresenter.swift
+++ MV(X)Patterns/GreatingPresenter.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+protocol GreatingPresenterProtocol {
+    init(view: GreatingViewProtocol, person: Person)
+    func showGreating()
+}
+
+class GreatingPresenter: GreatingPresenterProtocol {
+    
+    unowned let view: GreatingViewProtocol
+    let person: Person
+     
+    required init(view: GreatingViewProtocol, person: Person) {
+        self.view = view
+        self.person = person
+    }
+    
+    func showGreating() {
+        let greating = "Hello \(person.name) \(person.surname)!"
+        view.setGreating(greating)
+    }
+    
+}
--- MV(X)Patterns/GreatingViewController.swift
+++ MV(X)Patterns/GreatingViewController.swift
@@ -7,20 +7,34 @@
 
 import UIKit
 
+protocol GreatingViewProtocol: AnyObject {
+    func setGreating(_ greating: String)
+}
+
 class GreatingViewController: UIViewController {
     
     var person: Person!
+    
+    private var presenter: GreatingPresenterProtocol!
 
     @IBOutlet weak var userNameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         person = Person(name: "Tim", surname: "Cook")
+        presenter = GreatingPresenter(view: self, person: person)
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
-        userNameLabel.text = ("Hello, \(person.name) \(person.surname) !")
+        presenter.showGreating()
     }
+}
+
+extension GreatingViewController: GreatingViewProtocol {
+    func setGreating(_ greating: String) {
+        userNameLabel.text = greating
+    }
+    
     
 }
 
